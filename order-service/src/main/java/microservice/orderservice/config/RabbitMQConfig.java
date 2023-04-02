efe224a1d42e2d318d23d3313d349e44c4434fa8
azilzor <athirsonarceus@gmail.com>
2023-04-02 13:05:30 -0300

    feat: configuring rabbitmq on order service module

--- order-service/src/main/java/microservice/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/microservice/orderservice/config/RabbitMQConfig.java
@@ -1,7 +1,12 @@
 package microservice.orderservice.config;
 
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +22,29 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue orderQueue() {
-        return new Queue(queueName, true);
+        return new Queue(queueName, true); // Create queue with name
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(exchange); // Create exchange with name
+    }
+
+    @Bean
+    public Binding orderBinding() {
+        return BindingBuilder.bind(orderQueue()).to(orderExchange()).with(routingKey); // Bind queue to exchange with routing key
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter(); // Convert message to json format
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(); // Create rabbit template to send message to queue
+        rabbitTemplate.setMessageConverter(jsonMessageConverter()); // Set message converter to rabbit template to convert message to json
+
+        return rabbitTemplate;
     }
 }
